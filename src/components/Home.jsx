d42c1caa71b2bef4403ff4f65f2b63f36a42b2a2
Atharv garg <102018708+atharvgarg2010@users.noreply.github.com>
2022-09-25 22:00:08 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,10 +16,11 @@ function Home() {
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide><HomeItem s="Best Experience" img={Hm}/></SwiperSlide>
+      <SwiperSlide><HomeItem s="Open For Everyone" img="https://cdn.pixabay.com/photo/2017/09/19/17/57/gym-2765975_960_720.jpg"/></SwiperSlide>
+      
       <SwiperSlide><HomeItem s="Qualified Doctor" img="https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/></SwiperSlide>
       <SwiperSlide><HomeItem s="Affordable" img={Hme}/></SwiperSlide>
-      <SwiperSlide><HomeItem s="Open For Everyone" img="https://cdn.pixabay.com/photo/2017/09/19/17/57/gym-2765975_960_720.jpg"/></SwiperSlide>
+      <SwiperSlide><HomeItem s="Best Experience" img={Hm}/></SwiperSlide>
       
     </Swiper>
     </div>
